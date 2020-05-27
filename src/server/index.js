d9575e683705d1bb00a69cf87a03588fa284c59a
Abhinav Saurav <abhinavsaurav1@gmail.com>
2020-05-27 20:17:24 +0530

    build-dev working

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,35 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require("path");
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+const dotenv = require("dotenv");
+dotenv.config();
 
-console.log(__dirname)
+const mockAPIResponse = require("./mockAPI.js");
+var aylien = require("aylien_textapi");
 
-app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+//console.log(`Your API key is ${process.env.API_KEY}`);
+
+var textapi = new aylien({
+	application_id: process.env.API_ID,
+	application_key: process.env.API_KEY,
+});
+
+app.use(express.static("dist"));
+
+console.log(__dirname);
+
+app.get("/", function (req, res) {
+	res.sendFile("dist/index.html");
+	// res.sendFile(path.resolve("src/client/views/index.html"));
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+app.listen(8081, function () {
+	console.log("Example app listening on port 8080!");
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+app.get("/test", function (req, res) {
+	res.send(mockAPIResponse);
+});
